9581ab45d5dae23f9601781aa19272a126deedbe
Kim Malale <kimalale@hotmail.com>
2023-05-23 03:10:45 +0200

    Added functionality test

--- main.rb
+++ main.rb
@@ -1,1 +1,33 @@
 require './lib/binary-search-tree'
+
+
+
+node = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+node.insert(2)
+node.insert(10)
+node.insert(2)
+node.insert(6999)
+node.pretty_print
+node.delete(4)
+node.pretty_print
+
+
+
+print node.inorder
+print "\n"
+print node.preorder
+print "\n"
+print node.postorder
+print "\n"
+print node.inorder { |value| value <= 10}
+print "\n"
+print node.preorder { |value| value <= 10}
+print "\n"
+print node.postorder { |value| value <= 10}
+
+
+
+
+
+
